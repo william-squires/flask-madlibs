a8d4f67b3b1069c4e2e7abd4bf8ea21e656cbba6
William Squires <william.squiresii@gmail.com>
2023-03-08 18:22:29 -0600

    we can now submit and display a single madlib

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/questions")
+def generate_madlib_form():
+    """Generates an html form given the prompts from `silly_story`."""
+
+    return render_template("questions.html", words=silly_story.prompts)
+
+
+@app.get("/results")
+def display_madlib_result():
+    """Given user form data, fill in their madlib and show it."""
+    
+    response = request.args
+    story = silly_story.generate(response)
+    return render_template("results.html", story_out=story)
